285c40ccfc860fa4fbbae52c12f9638ffbc16f3d
Óscar M. Lage <info@oscarmlage.com>
2022-03-18 13:18:19 +0100

    Delete not-working code, templates related
    Templates are still WIP

--- main.go
+++ main.go
@@ -2,21 +2,8 @@ package main
 
 import (
 	"fmt"
-	"github.com/labstack/echo/v4"
 	"github.com/oscarmlage/wikingo/server"
-	"html/template"
-	"io"
 )
-
-// Define the template registry struct
-type TemplateRegistry struct {
-	templates *template.Template
-}
-
-// Implement e.Renderer intercace
-func (t *TemplateRegistry) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
-	return t.templates.ExecuteTemplate(w, name, data)
-}
 
 func main() {
 	fmt.Println("Starting wikingo...")
