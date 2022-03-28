10ed5858e85de064c0234c7bd056ba1561a6bf88
Óscar M. Lage <info@oscarmlage.com>
2022-03-28 21:19:36 +0200

    Add: version flag (-v)

--- main.go
+++ main.go
@@ -3,14 +3,26 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 
 	"github.com/oscarmlage/wikingo/server"
 )
 
+var (
+	app_name    = "Wikingo"
+	app_version = "0.0.1"
+)
+
 func main() {
-	fmt.Println("Starting wikingo...")
 	flag_debug := flag.Bool("d", false, "Enable debug mode")
+	flag_version := flag.Bool("v", false, "Show version")
 	flag.Parse()
+
+	// Version
+	if *flag_version {
+		fmt.Printf("%s - v%s\n", app_name, app_version)
+		os.Exit(0)
+	}
 
 	// Logger
 	server.InitLogger(*flag_debug)
@@ -18,5 +30,6 @@ func main() {
 
 	fmt.Println("Starting wikingo...")
 	fmt.Printf("Debug mode: %t\n", *flag_debug)
+	fmt.Printf("Version: %t\n", *flag_version)
 	server.Serve()
 }
